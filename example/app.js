77413638a8478a92bbc55eaf2d576aa040a0c1cb
Po-Ying Chen <poying.me@gmail.com>
2013-09-10 07:51:01 +0800

    把事件轉為大寫，避免指派 error 的時候把 error throw 出來

--- example/app.js
+++ example/app.js
@@ -4,14 +4,15 @@ var path = require('path');
 var Logger = require('../lib/logger');
 var logger = Logger.create('info', path.join(__dirname, 'log'), '%s.log');
 
-logger.on('info', function () {
+logger.on('INFO', function () {
   console.log('on info');
 });
 
-logger.on('log', function () {
+logger.on('LOG', function () {
   console.log('on log');
 });
 
 logger.debug('debug message');
 logger.info('info message');
 logger.notice('notice message');
+logger.error('error message');
